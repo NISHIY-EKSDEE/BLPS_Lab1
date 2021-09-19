6848ab08c77dfdb041db048a9aa5625888ac7333
Wieceslaw <slava.astra@yahoo.com>
2021-09-20 00:57:34 +0300

    Remove unused code

--- src/main/kotlin/com/example/lab1/controller/ProductController.kt
+++ src/main/kotlin/com/example/lab1/controller/ProductController.kt
@@ -48,25 +48,6 @@ class ProductController(
         return SellerProductResponseAssembler.buildDto(product)
     }
 
-//    /**
-//     * Метод, который позволяет пользователю с ролью CLIENT найти информацию о продукте, используя
-//     * набор тегов.
-//     */
-//    @GetMapping("/bytags")
-//    fun bytags(@RequestBody tagsForm : TagsForm) : ResponseEntity<Collection<SellerProductDTO>> {
-//        val listOfTags: MutableSet<TagEntity> = HashSet()
-//
-//        for (tag in tagsForm.tags) {
-//            val tagEntities = tagRepo.findAllByName(tag)
-//            listOfTags.addAll(tagEntities)
-//        }
-//
-//        val productsByTags = productTagService.getProductsByTags(listOfTags)
-//        val productSellerDTOs = productTagService.getSellerProductsDTOByProducts(productsByTags)
-//
-//        return ResponseEntity(productSellerDTOs, HttpStatus.OK)
-//    }
-
     /**
      * Метод, который доступен только пользователю с ролью SHOP. Позволяет ДОБАВИТЬ
      * новый продукт.
